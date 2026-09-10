dd3e081fcd03a9ea3dd0ad0bafa16cc64eda9b1f
Romuald Vallarcher <romuald@vallarcher.net>
2026-09-10 17:01:33 +0200

    Suivi série par série : validation, séries partielles, repos automatique

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache hors ligne : réseau d'abord (pour recevoir les mises à jour), cache si pas de connexion.
-const CACHE = 'muscu-v9';
+const CACHE = 'muscu-v10';
 const FILES = ['./', './index.html', './manifest.json', './icon-180.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => {
