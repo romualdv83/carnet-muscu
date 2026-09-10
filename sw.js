1db8f3a7182518e16b8b82e1c1036f2ccd323034
Romuald Vallarcher <romuald@vallarcher.net>
2026-09-10 16:34:05 +0200

    Menu Mouvement : bibliothèque proposée directement ; mises à jour plus rapides

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request).then(r => { const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return r; })
+    fetch(e.request, {cache: 'no-cache'}).then(r => { const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return r; })
       .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
